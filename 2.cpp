bea65b056a0c8df9a3d7fe1530082bb0882a7648
SvetlanaHayrapetyan <153418228+SvetlanaHayrapetyan@users.noreply.github.com>
2024-03-31 12:21:06 +0400

    2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,1 +1,31 @@
 // 2.     Ներածել սիմվոլային տող (առավելագույնը՝ 80 սիմվոլ)։ Ձևափոխել տողը փոք՚՚րատառ լատիներեն տառերը դարձնելով մեծատառ։ Ձևափոխությունը իրականացնել ֆունկցիայի միջոցով։
+
+int main()
+{
+   int n;
+   cout << "Enter the size of the array: ";
+   cin >> n;
+
+   char* dynamicArray = new char[n];
+
+   std::cout << "Enter " << n << " characters: ";
+   for (int i = 0; i < n; ++i) {
+       cin >> dynamicArray[i];
+
+       if (dynamicArray[i] == 'x') {
+           dynamicArray[i] = 'k';
+           dynamicArray[i + 1] = 's';
+           i++;
+       }
+   }
+   cout << "Modified Dynamic Array: ";
+   for (int i = 0; i < n; ++i) {
+       cout << dynamicArray[i] << " ";
+   }
+   cout << std::endl;
+
+   delete[] dynamicArray;
+
+   return 0;
+}
+
